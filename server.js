937a010f7e6220b0da0733a287f3547e14a47aad
anees05 <anees.adams05@gmail.com>
2019-08-08 16:28:02 -0400

    Set up server

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+
+
+var app = express();
+var PORT = process.env.PORT || 8040;
+
+app.use(express.static(path.join(__dirname, './app/public')));
+
+
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+app.listen(PORT, function () {
+    console.log('BFFL Finder app is listening on PORT: ' + PORT);
+});
