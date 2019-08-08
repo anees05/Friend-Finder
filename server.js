a9006b20d7d59f12fc39b32a246b3d61be802a01
anees05 <anees.adams05@gmail.com>
2019-08-08 18:22:54 -0400

    Fix issue with not retrieving json data

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var app = express();
 var PORT = process.env.PORT || 8040;
 
 app.use(express.static(path.join(__dirname, './app/public')));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 require(path.join(__dirname, './app/routing/apiRoutes'))(app);
 require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
